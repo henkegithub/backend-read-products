7b340921ac146ef120cdd3e52e80fbb1d7658223
Michael Henke <michaelh1995@gmx.de>
2023-07-26 09:37:52 +0200

    backend-create-products

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -3,10 +3,18 @@ import Product from "@/db/models/Product";
 
 export default async function handler(request, response) {
   await dbConnect();
+
   if (request.method === "GET") {
     const products = await Product.find();
-    return response.status(200).json(products);
-  } else {
-    return response.status(405).json({ message: "Method not allowed" });
+    response.status(200).json(products);
+    return;
   }
+  if (request.method === "POST") {
+    const productData = request.body;
+    await Product.create(productData);
+
+    response.status(201).json({ message: "status: Product created." });
+    return;
+  }
+  response.status(405).json({ message: "Method not allowed" });
 }
